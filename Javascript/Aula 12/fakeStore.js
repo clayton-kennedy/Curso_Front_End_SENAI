cd1f81878deaf0eee807f5042fd45f39701fc7b2
CK <clayton-kennedy@hotmail.com>
2023-09-26 11:45:35 -0300

    Aula

--- Javascript/Aula 12/fakeStore.js
+++ Javascript/Aula 12/fakeStore.js
@@ -5,11 +5,11 @@ const imagemProduto = document.querySelector('#imagemProduto');
 const descricaoProduto = document.querySelector('#descricaoProduto');
 const precoProduto = document.querySelector('#precoProduto');
 
-fetch('https://fakestoreapi.com/products?limit=100')
+fetch('https://fakestoreapi.com/products/category/electronics?limit=100')
   .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    const novoHTML = data.map(item => 
+  .then(dadosProdutos => {
+    console.log(dadosProdutos);
+    const novoHTML = dadosProdutos.map(item => 
      `
       <div id="produtos">
       <div class="card">
@@ -18,9 +18,35 @@ fetch('https://fakestoreapi.com/products?limit=100')
       <div class="descricao">
       <p id="descricaoProduto">${item.description}</p>
       <h4 id="precoProduto">R$ ${item.price}</h4>
+      <button id="comprar">Comprar</button>
       </div>
       </div>
     `
     )
     produtos.innerHTML += novoHTML;
   })
+
+
+/* RESOLUÇÃO DO PROFESSOR 
+
+  fetch('https://fakestoreapi.com/products/category/electronics?limit=100')
+  .then(response => response.json())
+  .then(dadosProdutos => {
+
+    dadosProdutos.forEach(dadosProdutos => {
+      produtos.innerHTML += 
+      `     
+      <div id="produtos">
+      <div class="card">
+      <span id="nomeProduto">${dadosProdutos.title}</span>
+      <img id="imagemProduto" src="${dadosProdutos.image}" alt="Imagem do produto">
+      <div class="descricao">
+      <p id="descricaoProduto">${dadosProdutos.description}</p>
+      <h4 id="precoProduto">R$ ${dadosProdutos.price}</h4>
+      <button id="comprar">Comprar</button>
+      </div>
+      </div>
+      `;
+    })
+  });
+*/
